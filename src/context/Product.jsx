5ba64e65c10a79a06232617566eadd3c8eedd5d4
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-09 17:54:41 +0100

    add methods for products

--- src/context/Product.jsx
+++ src/context/Product.jsx
@@ -1,45 +1,75 @@
 import { createContext, useContext, useEffect, useState } from "react";
 import { instance } from "../utils/api/axios";
+import { AuthContext } from "./Auth";
+import Swal from "sweetalert2";
 
 const ProductContext = createContext();
 
 export const useProductContext = () => useContext(ProductContext);
 
+const Toast = Swal.mixin({
+  toast: true,
+  position: 'bottom-end',
+  showConfirmButton: false,
+  timer: 2000,
+  timerProgressBar: true,
+  didOpen: (toast) => {
+    toast.addEventListener('mouseenter', Swal.stopTimer)
+    toast.addEventListener('mouseleave', Swal.resumeTimer)
+  }
+})
+
+
 export const ProductProvider = ({ children }) => {
+
   const [products, setProducts] = useState([]);
   const [categories, setCategories] = useState([]);
-  const { auth } = useProductContext();
+  const [currentPage, setCurrentPage] = useState(1)
+  const { auth } = useContext(AuthContext);
 
-
-  const FetchAllProducts = async () => {
-    await instance.get("/products").then(({ data }) => {
-      setProducts(data.products);
+  const fetchAllProducts = async () => {
+    await instance.get(`/products?page=${currentPage}`).then(({ data }) => {
+      setProducts(data);
     });
   };
 
-  const FetchAllCategories = async () => {
-    await instance.get('/categories').then((response) => {
-      setCategories(response.data)
+  const fetchAllCategories = async () => {
+    await instance.get('/categories').then(({ data }) => {
+      setCategories(data)
+    })
+  };
+
+  const fetchMyProducts = async () => {
+    await instance.get(`/products/user-products?page=${currentPage}`).then(({ data }) => {
+      setProducts(data)
     })
   };
 
   const addProduct = async (newProduct) => {
     await instance.post(`/products`, newProduct, {
-      headers: { Authorization: `Bearer ${auth.token}` }
-    }).then(({ data }) => {
-      setProducts(data.products);
+      headers: { Authorization: `Bearer ${auth.token}`, "Content-Type": "multipart/form-data" }
+    }).then(() => {
+      fetchMyProducts()
+      Toast.fire({
+        icon: 'success',
+        title: 'Signed in successfully'
+      })
     })
       .catch((e) => {
         console.log(e);
       });
   };
 
-  const updateProduct = async (productId, updatedProduct) => {
-    await instance.post(`/products/${productId}`, updatedProduct, {
-      headers: { Authorization: `Bearer ${auth.oken}` },
+  const updateProduct = async (form) => {
+    await instance.post(`/products/${form.id}`, form, {
+      headers: { Authorization: `Bearer ${auth.oken}`, ContentType: 'multipart/form-data' },
       _methode: 'PUT'
     }).then(({ data }) => {
       setProducts(data.products);
+      Toast.fire({
+        icon: 'success',
+        title: 'Signed in successfully'
+      })
     })
       .catch((e) => {
         console.log(e);
@@ -49,8 +79,12 @@ export const ProductProvider = ({ children }) => {
   const deleteProduct = async (productId) => {
     await instance.delete(`/products/${productId}`, {
       headers: { Authorization: `Bearer ${auth.token}` }
-    }).then(({ data }) => {
-      setProducts(data.products);
+    }).then(() => {
+      setProducts(products.filter(p => p.id !== productId))
+      Toast.fire({
+        icon: 'success',
+        title: 'Signed in successfully'
+      })
     })
       .catch((e) => {
         console.log(e);
@@ -67,8 +101,7 @@ export const ProductProvider = ({ children }) => {
   };
 
   useEffect(() => {
-    FetchAllProducts();
-    FetchAllCategories();
+    fetchAllCategories();
   }, []);
 
   return (
@@ -76,10 +109,14 @@ export const ProductProvider = ({ children }) => {
       value={{
         products,
         categories,
+        currentPage,
+        setCurrentPage,
         addProduct,
         updateProduct,
         deleteProduct,
         searchProducts,
+        fetchMyProducts,
+        fetchAllProducts
       }}
     >
       {children}
