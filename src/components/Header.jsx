59dd4230cbecf30cc05b9b2c69aa30f276f86335
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-14 23:34:59 +0100

    recreate navbar with material tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,57 +1,200 @@
-import { Fragment, useContext, useState } from 'react';
+import { useState, useEffect, createElement } from 'react';
+import { useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import { AuthContext } from '../context/Auth';
+import { selectCurrentUser } from '../features/auth/authSlice';
 
-const Header = () => {
-    const [showDropMenu, setShowDropMenu] = useState(false)
-    const { auth, logout } = useContext(AuthContext);
+import {
+    Navbar,
+    Collapse,
+    Typography,
+    Button,
+    IconButton,
+    Menu,
+    MenuHandler,
+    Avatar,
+    MenuList,
+    MenuItem,
+} from "@material-tailwind/react";
+
+import {
+    Bars3Icon,
+    XMarkIcon,
+    Cog6ToothIcon,
+    InboxArrowDownIcon,
+    LifebuoyIcon,
+    PowerIcon,
+    UserCircleIcon,
+    ChevronDownIcon
+} from '@heroicons/react/24/solid';
+
+
+const profileMenuItems = [
+    {
+        label: "My Profile",
+        icon: UserCircleIcon,
+        link: "/profile"
+    },
+    {
+        label: "Dashboard",
+        icon: Cog6ToothIcon,
+        link: "/dashboard"
+    },
+    {
+        label: "Inbox",
+        icon: InboxArrowDownIcon,
+        link: "/inbox"
+    },
+    {
+        label: "Help",
+        icon: LifebuoyIcon,
+        link: "/help"
+    },
+    {
+        label: "Sign Out",
+        icon: PowerIcon,
+        link: "/signout"
+    },
+];
+
+const ProfileMenu = () => {
+
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+    const closeMenu = () => setIsMenuOpen(false);
 
     return (
-        <Fragment>
-            <nav className="bg-primary dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 ">
-                <div className="reactive w-11/12  flex flex-wrap items-center justify-between mx-auto p-4 px-4">
-                    <Link to="/" className="flex items-center">
-                        <span className="self-center text-2xl font-bold text-white whitespace-nowrap">LOGO</span>
-                    </Link>
-                    <div className="flex md:order-2">
-                        {
-                            auth?.token != '' ?
-                                <>
-                                    <button className="flex mx-3 text-sm bg-gray-800 rounded-full md:mr-0 focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600" type="button" onClick={() => setShowDropMenu(!showDropMenu)}>
-                                        <span className="sr-only">Open user menu</span>
-                                        <span className="w-8 h-8 bg-gray-200 rounded-full" alt="user photo" ></span>
-                                    </button>
-                                    <div id="dropdownAvatar" className={`${showDropMenu ? 'block' : 'hidden'} absolute top-20 right-10 z-10 bg-white divide-y divide-gray-100 rounded-lg shadow w-56`}>
-                                        <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
-                                            <div>{auth?.user.name}</div>
-                                            <div className="font-medium truncate">{auth?.user.email}</div>
-                                        </div>
-                                        <ul className="py-2 text-sm text-gray-700 dark:text-gray-200">
-                                            <li>
-                                                <Link to="/profile" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Profile</Link>
-                                            </li>
-                                            <li>
-                                                <Link to="/dashboard" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</Link>
-                                            </li>
-                                        </ul>
-                                        <div className="py-2">
-                                            <button className="block px-4 py-2 text-md font-semibold text-red-500 hover:bg-gray-100 w-full" onClick={() => logout()}>Log out</button>
-                                        </div>
-                                    </div>
-                                </> :
-                                <>
-                                    <Link to={'auth/login'} className="text-white font-semibold text-lg hover:underline underline-offset-2">
-                                        Log in <span aria-hidden="true">&rarr;</span>
-                                    </Link>
-                                </>
-                        }
-                    </div>
-                    <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky"></div>
-                </div>
-            </nav>
+        <Menu open={isMenuOpen} handler={setIsMenuOpen} placement="bottom-end">
+            <MenuHandler>
+                <Button
+                    variant="text"
+                    className="flex items-center gap-1 rounded-full py-0.5 pr-2 pl-0.5 lg:ml-auto"
+                >
+                    <Avatar
+                        variant="circular"
+                        size="sm"
+                        alt="tania andrew"
+                        className="border-0 p-0.5 w-12 rounded-full"
+                        src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80"
+                    />
+                    <ChevronDownIcon
+                        strokeWidth={2.5}
+                        className={`h-3 w-3 transition-transform ${isMenuOpen ? "rotate-180" : ""}`}
+                    />
+                </Button>
+            </MenuHandler>
+            <MenuList key={Math.random(1)} className="p-3 z-20 w-48 flex flex-col gap-2 right-0">
+                {profileMenuItems.map(({ label, icon, link }, key) => {
+                    const isLastItem = (key === profileMenuItems.length - 1);
+                    return (
+                        <>
+                            <Link
+                                to={link}
+                                key={label}
+                            >
+                                <MenuItem
+                                    onClick={closeMenu}
+                                    className={`flex items-center py-1 gap-2 rounded 
+                                        ${isLastItem ? "hover:bg-gray-500/10 focus:bg-gray-500/10 active:bg-gray-500/10" : "hover:bg-gray-500/10 focus:bg-gray-500/10 active:bg-gray-500/10"}`}
+                                >
 
-        </Fragment >
+                                    {createElement(icon, {
+                                        className: `h-5 w-5 ${isLastItem ? "text-red-500" : ""}`,
+                                        strokeWidth: 2,
+                                    })}
+                                    <Typography
+                                        as="span"
+                                        variant="h1"
+                                        className="font-normal"
+                                        color={isLastItem ? "red" : "inherit"}
+                                    >
+                                        {label}
+                                    </Typography>
+                                </MenuItem>
+                            </Link>
+                        </>
+                    );
+                })}
+            </MenuList>
+        </Menu>
     )
+}
+
+export function Header() {
+    const [openNav, setOpenNav] = useState(false);
+
+    useEffect(() => {
+        window.addEventListener(
+            "resize",
+            () => window.innerWidth >= 960 && setOpenNav(false)
+        );
+    }, []);
+
+    const user = useSelector(selectCurrentUser)
+
+    return (
+        <Navbar className="px-8 py-3 bg-primary rounded-none border-0">
+            <div className="flex items-center justify-between text-blue-gray-900">
+                <Typography
+                    as="a"
+                    variant="h6"
+                    className="mr-4 cursor-pointer py-1.5 lg:ml-2"
+                >
+                    Logo
+                </Typography>
+                <div className="hidden lg:block">
+                    {/* <NavList /> */}
+                </div>
+                <div className="hidden gap-2 lg:flex">
+                    {
+                        user ?
+                            <ProfileMenu /> :
+                            <>
+                                <Link to='auth/login'>
+                                    <Button variant="text" size="sm" color="blue-gray">
+                                        Sign In
+                                    </Button>
+                                </Link>
+
+                                <Button variant="gradient" size="sm">
+                                    Sign Up
+                                </Button>
+                            </>
+                    }
+                </div>
+
+                <IconButton
+                    variant="text"
+                    color="blue-gray"
+                    className="lg:hidden"
+                    onClick={() => setOpenNav(!openNav)}
+                >
+                    {openNav ? (
+                        <XMarkIcon className="h-6 w-6" strokeWidth={2} />
+                    ) : (
+                        <Bars3Icon className="h-6 w-6" strokeWidth={2} />
+                    )}
+                </IconButton>
+            </div>
+            <Collapse open={openNav}>
+                {/* <NavList /> */}
+                <div className="flex w-full flex-nowrap items-center gap-2 lg:hidden">
+                    {user ?
+                        <ProfileMenu /> :
+                        <>
+                            <Link to='auth/login'>
+                                <Button variant="outlined" size="sm" color="blue-gray" fullWidth>
+                                    Sign In
+                                </Button>
+                            </Link>
+
+                            <Button variant="gradient" size="sm" fullWidth>
+                                Sign Up
+                            </Button>
+                        </>
+                    }
+                </div>
+            </Collapse>
+        </Navbar>
+    );
 }
 
 export default Header
