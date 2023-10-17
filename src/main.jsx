b62f68addbbdb4ee044cfadc7097a8c2c017adc5
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-17 03:32:45 +0100

    import store redux of main

--- src/main.jsx
+++ src/main.jsx
@@ -1,27 +1,35 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import "./index.css";
+
 import { ProductProvider } from "./context/Product.jsx";
-import Auth from "./Pages/auth";
 import AuthProvider from "./context/Auth.jsx";
-import ErrorPage from "./components/404.jsx";
+
+// import ErrorPage from "./components/404.jsx";
+import Auth from "./Pages/auth";
+import App from "./App.jsx";
+
+import { Provider } from 'react-redux'
+import store from './app/store';
+
+import "./index.css";
 
 const root = createRoot(document.getElementById("root"));
 
 root.render(
   <StrictMode>
-    <Router>
-      <AuthProvider>
-        <ProductProvider>
-          <Routes>
-            <Route path="/*" element={<App />} />
-            <Route path='/auth/*' element={<Auth />} />
-            {/* <Route path="/*" element={<ErrorPage />} /> */}
-          </Routes>
+    <Provider store={store}>
+      <Router>
+        <AuthProvider>
+          <ProductProvider>
+        <Routes>
+          <Route path="/*" element={<App />} />
+          <Route path='/auth/*' element={<Auth />} />
+          {/* <Route path="/*" element={<ErrorPage />} /> */}
+        </Routes>
         </ProductProvider>
-      </AuthProvider>
-    </Router>
+        </AuthProvider>
+      </Router>
+    </Provider>
   </StrictMode>
 );
