58801338efbcd70f917d3ccb350f5448698eaa71
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-17 14:11:59 +0100

    distribution of context api

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-
-import { ProductProvider } from "./context/Product.jsx";
-import AuthProvider from "./context/Auth.jsx";
-
 // import ErrorPage from "./components/404.jsx";
 import Auth from "./Pages/auth";
 import App from "./App.jsx";
@@ -20,15 +16,11 @@ root.render(
   <StrictMode>
     <Provider store={store}>
       <Router>
-        <AuthProvider>
-          <ProductProvider>
         <Routes>
           <Route path="/*" element={<App />} />
           <Route path='/auth/*' element={<Auth />} />
           {/* <Route path="/*" element={<ErrorPage />} /> */}
         </Routes>
-        </ProductProvider>
-        </AuthProvider>
       </Router>
     </Provider>
   </StrictMode>
--- src/utils/api/axios.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-
-const token = localStorage.getItem("token");
-
-export const instance = axios.create({
-  baseURL: "http://localhost:8000/api/",
-  headers: { Authorization: `Bearer ${token}` }
-});
