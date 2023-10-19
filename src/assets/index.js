26df0d45d3cad878bbfe2b397cc9ed71eaad8694
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-19 16:53:41 +0100

    create index for assets

---
+++ src/assets/index.js
@@ -0,0 +1,9 @@
+import BlogBg from "./images/blog_hero_bg_1.webp";
+import LoginBg from "./images/login_bg.webp";
+
+import glassvr_1 from "./images/product.png/glassvr_1.png";
+import glassvr_2 from "./images/product.png/glassvr_2.png";
+
+export { BlogBg, LoginBg };
+
+export const PNGS = { glassvr_1, glassvr_2 };
