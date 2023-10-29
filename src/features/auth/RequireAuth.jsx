b7dccbd87d9848ab362ca9812b4cdbe0750ffbff
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-29 04:57:52 +0100

    fix call of current token

--- src/features/auth/RequireAuth.jsx
+++ src/features/auth/RequireAuth.jsx
@@ -1,9 +1,10 @@
 import { Navigate, Outlet, useLocation } from "react-router";
 import { selectCurrentToken } from "./authSlice";
+import { useSelector } from "react-redux";
 
 export const RequireAuth = () => {
-  const token = selectCurrentToken()
+  const token = useSelector(selectCurrentToken);
   const location = useLocation();
-  
-  return token ? (<Outlet />) : (<Navigate to="/" state={{ from: location }} replace />);
+
+  return token ? (<Outlet />) : (<Navigate to="/home" state={{ from: location }} replace />);
 };
