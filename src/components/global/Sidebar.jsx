072fcb35ca7310821bcbfa7a6752d9f0f66a790e
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-29 05:00:32 +0100

    remove logout from sidebar

--- src/components/global/Sidebar.jsx
+++ src/components/global/Sidebar.jsx
@@ -12,7 +12,7 @@ import {
     AccordionBody,
 } from "@material-tailwind/react";
 
-import { PowerIcon, ChevronDownIcon, Bars3BottomLeftIcon, Bars3BottomRightIcon } from "@heroicons/react/24/solid";
+import { ChevronDownIcon, Bars3BottomLeftIcon, Bars3BottomRightIcon } from "@heroicons/react/24/solid";
 
 function Sidebar() {
     const [open, setOpen] = useState(0);
@@ -25,12 +25,12 @@ function Sidebar() {
 
     useEffect(() => { if (openSidebar === true) setOpen(0) }, [openSidebar])
     return (
-        <Card className={`!h-full fixed z-30 px-1 shadow-xl shadow-blue-gray-900/5 mt-20 rounded-none ${openSidebar ? '!w-20' : '!w-64'}`}>
+        <Card className={`!h-full fixed z-30 px-1 shadow-xl shadow-blue-gray-900/5 mt-16 rounded-none bg-primary/80 ${openSidebar ? '!w-20' : '!w-64'}`}>
             <List>
-                <ListItem className={`flex ${openSidebar ? 'w-14' : ''}`}>
-                    <ListItemPrefix className={`!flex flex-1 !items-end !justify-end `} onClick={() => setOpenSidebar(!openSidebar)}>
+                <ListItem className={`flex mt-4 ${openSidebar ? 'w-14' : ''}`}>
+                    <ListItemPrefix className={`!flex flex-1 !items-end !justify-end`} onClick={() => setOpenSidebar(!openSidebar)}>
                         {
-                            openSidebar ? <Bars3BottomLeftIcon className={`w-7`} /> : <Bars3BottomRightIcon className={`w-7`} />
+                            openSidebar ? <Bars3BottomLeftIcon color="white" className={`w-7`} /> : <Bars3BottomRightIcon color="white" className={`w-7`} />
                         }
                     </ListItemPrefix>
                 </ListItem>
@@ -44,6 +44,7 @@ function Sidebar() {
                                 icon={
                                     <ChevronDownIcon
                                         strokeWidth={2.5}
+                                        color="white"
                                         className={`mx-auto h-4 w-4 transition-transform ${open === i + 1 ? "rotate-180" : ""}`}
                                     />
                                 }
@@ -52,16 +53,16 @@ function Sidebar() {
                                 <ListItem className="p-0" selected={open === i + 1}>
                                     <AccordionHeader onClick={() => handleOpen(i + 1)} className="border-b-0 p-3 flex gap-2">
                                         <ListItemPrefix>
-                                            <item.icon className="w-6" />
+                                            <item.icon color="white" className="w-6" />
                                         </ListItemPrefix>
-                                        <Typography color="blue-gray" className={`mr-auto font-semibold text-blue-gray-700 text-[16px] ${!openSidebar ? 'blok' : 'hidden'}`}>
+                                        <Typography color="white" className={`mr-auto font-semibold text-[16px] ${!openSidebar ? 'blok' : 'hidden'}`}>
                                             {item.name}
                                         </Typography>
                                     </AccordionHeader>
                                 </ListItem>
 
                                 <AccordionBody className={`py-1 pl-8 ${open !== i + 1 ? 'hidden' : 'block'}`}>
-                                    <List className="p-0">
+                                    <List className="p-0 text-white">
                                         {
                                             item.children.map((child, i) => <Link to={child.link} key={`child-${i}`}><ListItem > {child.name}</ListItem ></Link>)
                                         }
@@ -69,12 +70,12 @@ function Sidebar() {
                                 </AccordionBody>
                             </Accordion >
                         } else {
-                            return <ListItem key={`parent-${i}`} className={`${openSidebar ? '!w-16' : ''}`}>
+                            return <ListItem key={`parent-${i}`} className={`hover:bg-none ${openSidebar ? '!w-16' : ''}`}>
                                 <Link to={item.link} className="flex gap-3">
                                     <ListItemPrefix>
-                                        <item.icon className={`w-6`} />
+                                        <item.icon color="gray-blue" className={`w-6`} />
                                     </ListItemPrefix>
-                                    <ListItemPrefix className={`${!openSidebar ? 'flex' : 'hidden'}`}>
+                                    <ListItemPrefix className={`text-gray-blue-50 ${!openSidebar ? 'flex' : 'hidden'}`}>
                                         {item.name}
                                     </ListItemPrefix>
                                 </Link>
@@ -82,17 +83,6 @@ function Sidebar() {
                         }
                     })
                 }
-
-                <hr className={` ${openSidebar ? 'w-16' : ''} my-2 border-blue-gray-200`} />
-
-                <ListItem className={`flex gap-2 text-red-600 font-semibold hover:bg-red-400/40 ${openSidebar ? 'w-16' : ''}`}>
-                    <ListItemPrefix>
-                        <PowerIcon className={`w-6`} />
-                    </ListItemPrefix>
-                    <ListItemPrefix className={`${!openSidebar ? 'blok' : 'hidden'}`}>
-                        Log Out
-                    </ListItemPrefix>
-                </ListItem>
             </List>
         </Card >
     );
