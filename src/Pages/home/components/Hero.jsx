c5af815c25297c07731f61a7b268574131665232
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-10-29 04:52:47 +0100

    edit hero home

--- src/Pages/home/components/Hero.jsx
+++ src/Pages/home/components/Hero.jsx
@@ -16,60 +16,61 @@ function Hero() {
                 </div>
             )}
         >
-            <div className="h-full bg-hero-bg-2 bg-cover bg-center flex justify-end" >
-                <div className="w-full bg-black/40 py-24">
+            <div className="bg-hero-bg-2 bg-cover bg-center flex justify-end" >
+                <div className="w-full bg-black/50 pt-56 pb-24">
                     <div className="h-full flex flex-col gap-16 items-center text-center">
-                        <div className="flex flex-col items-center gap-6">
-                            <Typography variant="h4" color="white" className="uppercase">
+                        <div className="flex flex-col items-center gap-4">
+                            <Typography variant="h5" color="white" className="uppercase text-blue-gray-50">
                                 New Reality
                             </Typography>
                             <Typography variant="h1" className="text-white lg:text-6xl">
-                                New impre<span className="text-purple-500">ssions</span>
+                                New impre<span className="text-tertiary">ssions</span>
                             </Typography>
-                            <Typography className="text-gray-300 md:text-xl">
+                            <Typography className="text-blue-gray-50 md:text-xl">
                                 Wear VR helmet and start new battles
                             </Typography>
-                            <Button className="rounded-full bg-purple-500 w-48 p-4">to get catalog</Button>
+                            <Button className="rounded-full bg-tertiary w-48 p-4">to get catalog</Button>
                         </div>
-
-                        <div className="flex flex-col md:flex-row justify-center items-center gap-6 h-24 w-full">
-                            <Card className="w-full bg-white/50 !p-0 h-full  max-w-[20rem] flex-row">
+                        <div className="flex flex-col md:flex-row justify-center items-center gap-6 h-24 mt-16 w-full">
+                            <Card className="w-full bg-blue-gray-200/60 !p-0 h-full  max-w-[20rem] flex-row">
                                 <CardHeader
                                     shadow={false}
                                     floated={false}
-                                    className="m-0 w-2/5 shrink-0 bg-blue-gray-600/80rounded-r-none p-1 h-full bg-cover"
+                                    className="m-0 w-1/2 shrink-0 bg-transparent p-1 h-full bg-cover"
                                     style={{ backgroundImage: `url(${PNGS.glassvr_1})` }}
                                 >
+                                    <span></span>
                                 </CardHeader>
                                 <CardBody className="pl-4 flex flex-col justify-center w-full">
-                                    <Typography variant="h6" color="white" className="uppercase ">
+                                    <Typography variant="h6" className="uppercase text-blue-gray-50">
                                         Oculus
                                     </Typography>
-                                    <Typography variant="h6" color="white" className="">
+                                    <Typography variant="h6" className="text-blue-gray-50">
                                         VR Glasses
                                     </Typography>
-                                    <Typography className=" font-extrabold text-purple-500">
+                                    <Typography className=" font-extrabold text-secondary">
                                         129.50 $
                                     </Typography>
                                 </CardBody>
                             </Card>
-                            <Card className="w-full bg-white/50 p-0 h-full max-w-[20rem] flex-row">
+                            <Card className="w-full bg-blue-gray-200/60 p-0 h-full max-w-[20rem] flex-row">
                                 <CardHeader
                                     shadow={false}
                                     floated={false}
-                                    className="m-0 w-2/5 shrink-0 bg-blue-gray-600/80rounded-r-none p-1 h-full bg-cover "
+                                    className="m-0 w-1/2 shrink-0 bg-transparent p-1 h-full bg-cover "
                                     style={{ backgroundImage: `url(${PNGS.glassvr_2})` }}
 
                                 >
+                                    <span></span>
                                 </CardHeader>
                                 <CardBody className="pl-4 flex flex-col justify-center w-full">
-                                    <Typography variant="h6" color="white" className="uppercase">
+                                    <Typography variant="h6" className="uppercase text-blue-gray-50">
                                         Oculus
                                     </Typography>
-                                    <Typography variant="h6" color="white" className="">
+                                    <Typography variant="h6" className="text-blue-gray-50">
                                         VR Glasses
                                     </Typography>
-                                    <Typography variant="h6" className=" font-extrabold text-purple-500">
+                                    <Typography variant="h6" className=" font-extrabold text-secondary">
                                         129.50 $
                                     </Typography>
                                 </CardBody>
@@ -78,7 +79,7 @@ function Hero() {
                     </div>
                 </div>
             </div>
-        </Carousel>
+        </Carousel >
     );
 }
 
