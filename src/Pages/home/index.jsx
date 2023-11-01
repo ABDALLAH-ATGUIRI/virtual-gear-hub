c21f4e205e1ea299692a6dd3f3ea4c11e4c6b7d8
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-02 00:05:19 +0100

    remove offers component

--- src/Pages/home/index.jsx
+++ src/Pages/home/index.jsx
@@ -1,19 +1,19 @@
 import FilterSideBar from '../../components/FilterSideBar';
 import ProductsSection from '../../components/sections/products.section';
+import ProcurementPanel from '../user/ProcurementPanel';
 import Hero from './components/Hero';
-import Offers from './components/Offers';
 
-const index = () => {
+const Index = () => {
     return (
         <div className="min-h-screen flex flex-col justify-start items-center gap-8">
             <Hero />
             <div id="services" className="w-full flex p-4">
                 <FilterSideBar />
                 <ProductsSection />
+                <ProcurementPanel />
             </div>
-            <Offers />
         </div>
     )
 }
 
-export default index
+export default Index
