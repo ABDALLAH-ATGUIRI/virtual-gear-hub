6c890b84def1f054e372752decd8a84d3886c754
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-06 16:07:46 +0100

    add new images for games

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,12 +1,33 @@
 import BlogBg from "./images/blog_hero_bg_1.webp";
 import LoginBg from "./images/login_bg.webp";
 
-import glassvr_1 from "./images/product.png/glassvr_1.png";
-import glassvr_2 from "./images/product.png/glassvr_2.png";
+import glassvr_1 from "./images/product/glassvr_1.png";
+import glassvr_2 from "./images/product/glassvr_2.png";
+
+import game_01 from "./images/games/game_01.webp";
+import game_02 from "./images/games/game_02.jpg";
+import game_03 from "./images/games/game_03.jpg";
+import game_04 from "./images/games/game_04.jpg";
+import game_05 from "./images/games/game_05.jpg";
+import game_06 from "./images/games/game_06.jpg";
+import game_07 from "./images/games/game_07.jpg";
+import game_08 from "./images/games/game_08.jpg";
 
 import error_page from "./svg/error_page.svg";
 
 export { BlogBg, LoginBg };
 
-export const PNGS = { glassvr_1, glassvr_2 };
 export const SVGS = { error_page };
+
+export const PRODUCTSIMG = {
+  glassvr_1,
+  glassvr_2,
+  game_01,
+  game_02,
+  game_03,
+  game_04,
+  game_05,
+  game_06,
+  game_07,
+  game_08
+};
