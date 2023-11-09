2ebccb4d12818c96d87e90e572431a134db67100
ABDALLAH-ATGUIRI <wahbidox@gmail.com>
2023-11-09 01:29:36 +0100

    change ui of products catalog

--- src/components/cards/product.card.jsx
+++ src/components/cards/product.card.jsx
@@ -1,39 +1,31 @@
-import { Card, CardBody, CardFooter, Typography, Button } from "@material-tailwind/react";
+import { Card, CardBody, CardFooter, Typography, Button, CardHeader } from "@material-tailwind/react";
 import { PlusCircleIcon } from '@heroicons/react/24/outline';
 import { PRODUCTSIMG } from "../../assets";
 
 export const ProductCard = ({ item }) => {
     return (
-        <Card color="transparent" className="relative flex flex-col justify-end h-[20rem] w-full max-w-[28rem] overflow-hidden shadow-md shadow-gray-800/10 [&>*]:bg-gray-700/10 text-center rounded-md  hover:border hover:border-secondary rounded-tl-lg">
-            <CardBody className='relative w-full flex flex-col items-center p-0'>
+        <Card color="transparent" className="relative flex flex-col justify-end h-[20rem] w-full max-w-[28rem] overflow-hidden shadow-md shadow-black/20 dark:shadow-white/20 dark:[&>*]:text-gray-200 [&>*]:text-gray-900 text-center rounded-md  hover:border hover:border-secondary rounded-tl-lg">
+            <CardHeader color="blue-gray" className="relative m-0 w-full h-32 rounded-sm shadow-lg shadow-white dark:shadow-gray-900 bg-transparent" >
                 <img
                     src={`${PRODUCTSIMG.glassvr_1}`}
                     alt="ui/ux review check "
-                    className="absolute z-10 left-0 -top-20 h-full w-full object-fit"
+                    className="absolute m-2 w-full h-full"
                 />
-                <div className="h-full px-4 mt-14">
-                    <Typography variant="h4" color="white"> {item.name} </Typography>
-                    <Typography color="gray" className="mt-1 font-normal text-xs" > {item.category.name} </Typography>
-                    <Typography color="white" className="mt-3 text-sm h-14" > {item.description} </Typography>
-                </div>
-            </CardBody>
-            <CardFooter className="p-0">
-                <Typography
-                    variant="paragraph"
-                    color="white"
-                    className="bg-secondary/60 absolute z-20 p-1 px-6 top-0 left-0 rounded-br-xl text-md font-semibold col-row-3"
-                >
+                <Typography variant="paragraph" color="white" className="bg-secondary/80 absolute z-20 p-1 px-6 top-0 left-0 rounded-br-xl text-md font-semibold col-row-3">
                     {item.price} $
                 </Typography>
-                <Button
-                    fullWidth
-                    variant="text"
-                    className="flex items-center justify-center gap-2 text-blue-gray-100"
-                >
-                    <PlusCircleIcon className="h-10 w-10 text-secondary" />
+            </CardHeader>
+            <CardBody className="p-0 pt-3 h-36">
+                <Typography variant="h4"> {item.name} </Typography>
+                <Typography color="gray" className="mt-1 font-normal text-xs" > {item.category.name} </Typography>
+                <Typography className="mt-3 text-sm h-14" > {item.description} </Typography>
+            </CardBody>
+            <CardFooter className="p-0">
+                <Button fullWidth variant="text" className="flex items-center justify-center gap-2 dark:text-gray-200 text-gray-900 hover:bg-secondary/10 rounded-none">
+                    <PlusCircleIcon className="h-8 w-8 !text-secondary" />
                     Order Now
                 </Button>
             </CardFooter>
-        </Card >
+        </Card>
     );
 }
--- src/components/sections/products.section.jsx
+++ src/components/sections/products.section.jsx
@@ -1,10 +1,70 @@
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 
-import { CardProduct } from '../cards/product.card'
+import { ProductCard } from '../cards/product.card'
 import { useFetchAllProductsMutation } from '../../features/products/productApiSlice';
 import { selectCurrentProducts, setProductsCredentials } from '../../features/products/productSlice';
+import { GamesCard } from '../cards/games.card';
+import { PRODUCTSIMG } from '../../assets';
 
+const GAMES = [
+    {
+        name: 'Product1',
+        category: 'Category1',
+        color: 'blue',
+        quantity: 2,
+        price: 100,
+        poster: PRODUCTSIMG.game_01
+    },
+    {
+        name: 'Product2',
+        category: 'Category2',
+        color: 'green',
+        quantity: 4,
+        price: 200,
+        poster: PRODUCTSIMG.game_02
+    },
+    {
+        name: 'Product2',
+        category: 'Category2',
+        color: 'red',
+        quantity: 6,
+        price: 300,
+        poster: PRODUCTSIMG.game_03
+    },
+    {
+        name: 'Product2',
+        category: 'Category2',
+        color: 'blue',
+        quantity: 8,
+        price: 400,
+        poster: PRODUCTSIMG.game_04
+    },
+    {
+        name: 'Product2',
+        category: 'Category2',
+        color: 'green',
+        quantity: 4,
+        price: 200,
+        poster: PRODUCTSIMG.game_05
+    },
+    {
+        name: 'Product2',
+        category: 'Category2',
+        color: 'red',
+        quantity: 6,
+        price: 300,
+        poster: PRODUCTSIMG.game_06
+    },
+    {
+        name: 'Product2',
+        category: 'Category2',
+        color: 'blue',
+        quantity: 8,
+        price: 400,
+        poster: PRODUCTSIMG.game_07
+    },
+]
 const ProductsSection = () => {
     const [fetch] = useFetchAllProductsMutation();
     const products = useSelector(selectCurrentProducts)
@@ -24,9 +84,15 @@ const ProductsSection = () => {
     }, [dispatch])
 
     return (
-        <section className="px-6 w-full grid grid-rows-2 xl:grid-cols-5 lg:grid-cols-3 grid-cols-2 justify-center gap-x-5 gap-y-6">
+        <section className="w-full grid grid-rows-4 2xl:grid-cols-6 xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 grid-cols-1 justify-center gap-x-6 gap-y-10">
             {products?.length ?
-                products.map((item, i) => <CardProduct key={i} item={item} />) :
+                products.map((item, i) => <ProductCard key={i} item={item} />) :
+                <div colSpan="7" className="text-center w-full col-span-4">
+                    <p className="text-gray-500 text-md font-semibold">No product found</p>
+                </div>
+            }
+            {GAMES?.length ?
+                GAMES.map((item, i) => <GamesCard key={i} item={item} />) :
                 <div colSpan="7" className="text-center w-full col-span-4">
                     <p className="text-gray-500 text-md font-semibold">No product found</p>
                 </div>
