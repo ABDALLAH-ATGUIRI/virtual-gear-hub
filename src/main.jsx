fffa83a48e81e2f06036863aee68b0ad26cd0a88
abdallah atguiri <abdallah.atguiri@gmail.com>
2024-10-16 01:33:23 +0100

    optimize main

--- src/main.jsx
+++ src/main.jsx
@@ -1,15 +1,18 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom";
+import React, { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Pages from "./Pages";
 
-import { Provider } from 'react-redux'
+import { Provider } from 'react-redux';
 import store from './app/store';
 
 import "./index.css";
 
-const root = createRoot(document.getElementById("root"));
+// Create root instance
+const rootElement = document.getElementById("root");
+const root = createRoot(rootElement);
 
+// Render the app with provider and routing setup
 root.render(
   <StrictMode>
     <Provider store={store}>
