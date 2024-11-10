51a752d664e0822a37b1e5126224636662500213
abdallah atguiri <abdallah.atguiri@gmail.com>
2024-11-10 02:00:15 +0100

    refactore DialogDefault

--- src/components/DialogDefault.jsx
+++ src/components/DialogDefault.jsx
@@ -1,37 +1,49 @@
+import PropTypes from "prop-types";
 import { Button, Dialog, DialogHeader, DialogBody, DialogFooter } from "@material-tailwind/react";
 import { useDispatch, useSelector } from "react-redux";
 import { closeDialog } from "@features/dialogsReducer";
+import { useCallback } from "react";
 
-function DialogDefault({ title, handle, children, dialogId, className }) {
-    const { isOpen } = useSelector((store) => store.dialogs[dialogId] || false)
-    const dispatch = useDispatch()
+function DialogDefault(props) {
+    const { title, handle, children: { body, footer }, dialogId, className } = props;
+    const isOpen = useSelector((store) => store.dialogs[dialogId] || false);
+    const dispatch = useDispatch();
+
+    const handleClose = useCallback(() => {
+        dispatch(closeDialog(dialogId));
+    }, [dispatch, dialogId]);
 
     return (
-        <Dialog open={isOpen || false} handler={() => dispatch(closeDialog(dialogId))} className={className}>
-            <DialogHeader>{title}</DialogHeader>
+        <Dialog open={isOpen} handler={handleClose} className={`dark:[&>_*]:bg-dark dark:[&>_*]:text-white ${className}`}>
+            <DialogHeader className="dark:text-white text-xl">{title}</DialogHeader>
             <DialogBody divider>
-                {children.body}
+                {body}
             </DialogBody>
             <DialogFooter>
-                {
-                    children.footer || <>
-                        <Button
-                            variant="outlined"
-                            color="red"
-                            onClick={() => dispatch(closeDialog(dialogId))}
-                            className="mr-1"
-                        >
+                {footer || (
+                    <>
+                        <Button variant="outlined" color="red" onClick={handleClose} className="mr-1">
                             Cancel
                         </Button>
-                        <Button variant="outlined" color="green" onClick={() => handle()}>
+                        <Button variant="outlined" color="green" onClick={handle}>
                             Confirm
                         </Button>
                     </>
-                }
-
+                )}
             </DialogFooter>
         </Dialog>
     );
 }
 
-export default DialogDefault
+DialogDefault.propTypes = {
+    title: PropTypes.string.isRequired,
+    handle: PropTypes.func,
+    children: PropTypes.shape({
+        body: PropTypes.node.isRequired,
+        footer: PropTypes.node
+    }).isRequired,
+    dialogId: PropTypes.string.isRequired,
+    className: PropTypes.string
+};
+
+export default DialogDefault;
