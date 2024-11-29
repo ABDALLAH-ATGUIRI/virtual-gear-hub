51f618020f6d12eeabe566bd69200e1b862721bc
abdallah atguiri <abdallah.atguiri@gmail.com>
2024-11-29 22:11:42 +0100

    create page dashboard

---
+++ src/Pages/Backoffice/Dashboard/index.jsx
@@ -0,0 +1,15 @@
+
+import "./index.css"
+import WelcomePanel from './components/WelcomePanel'
+
+function Dashboard() {
+  return (
+    <>
+      <WelcomePanel/>
+    </>
+  )
+}
+
+Dashboard.propTypes = {}
+
+export default Dashboard
