9ca4bb5c91a1d8e675a4380360e493aba0efdd8d
abdallah atguiri <abdallah.atguiri@gmail.com>
2024-11-29 22:21:13 +0100

    create default component

---
+++ src/components/Default.jsx
@@ -0,0 +1,11 @@
+const Default = () => {
+	return (
+		<div className="h-screen flex justify-center items-center text-6xl font-extrabold">
+			Hello, Are you ready to start?
+		</div>
+	);
+};
+
+Default.propTypes = {};
+
+export default Default;
